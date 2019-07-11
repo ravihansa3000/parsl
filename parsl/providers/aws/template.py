ef92dad6863781053ae9cfbdb5890bc4695d23d3
Ben Clifford <benc@hawaga.org.uk>
2019-07-11 01:16:34 +0000

    hack aws from git, copied from master patch stack

--- parsl/providers/aws/template.py
+++ parsl/providers/aws/template.py
@@ -2,8 +2,14 @@ template_string = """#!/bin/bash
 cd ~
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y python3 python3-pip libffi-dev g++ libssl-dev
-pip3 install numpy scipy parsl
+apt-get install -y python3 python3-pip libffi-dev g++ libssl-dev git
+pip3 install numpy scipy
+git clone https://github.com/Parsl/parsl/
+pushd parsl
+git checkout 86120f5364548d37d8fd9a90a6628b0c6986b772
+pip3 install .
+popd
+
 $worker_init
 
 $user_script
